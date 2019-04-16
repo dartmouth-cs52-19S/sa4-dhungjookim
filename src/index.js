5e37a5cbf024606c1da987ffd27767430649481e
DhungJoo Kim <dhungjoo.kim.19@dartmouth.edu>
2019-04-15 22:23:39 -0400

    finished SA4 with no extra credit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './style.scss';
+import debounce from 'lodash.debounce';
+import youtubeSearch from './youtube-api';
+import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
+import VideoList from './components/video_list';
 
-const App = () => <div className="test">All the REACT are belong to us!</div>;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+    this.search = debounce(this.search, 300);
+    this.search('pixar');
+  }
+
+    search = (text) => {
+      youtubeSearch(text).then((videos) => {
+        this.setState({
+          videos,
+          selectedVideo: videos[0],
+        });
+      });
+    }
+
+    render() {
+      return (
+        <div>
+          <div id="search-bar">
+            <SearchBar onSearchChange={this.search} />
+          </div>
+          <div id="video-section">
+            <VideoDetail video={this.state.selectedVideo} />
+            <VideoList onVideoSelect={selectedVideo => this.setState({ selectedVideo })} videos={this.state.videos} />
+          </div>
+        </div>
+      );
+    }
+}
+
 
 ReactDOM.render(<App />, document.getElementById('main'));
